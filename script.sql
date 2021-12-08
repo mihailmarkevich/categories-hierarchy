9ca150962f3527d94d62857189e6e375ab5452a5
mihailmarkevich <mihail.markevich.nox@gmail.com>
2021-12-08 21:54:42 +0200

    Categories hierarchy collect example

--- script.sql
+++ script.sql
@@ -1,10 +1,12 @@
 CREATE TABLE categories (
-  `id` INT PRIMARY KEY AUTO_INCREMENT
-  `name` VARCHAR(255) NOT NULL
+  `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `parent_id` INT UNSIGNED NULL,
+  `level` INT NOT NULL
 );
 
-CREATE TABLE categories_to_categories (
-  `category_id` INT NOT NULL,
-  `parent_category_id` INT NOT NULL,
-  `level` INT NOT NULL,
-);
+-- DATA
+INSERT INTO categories (`name`, `parent_id`, `level`) VALUES ('Техника', 0, 0);
+INSERT INTO categories (`name`, `parent_id`, `level`) VALUES ('Гаджеты', 1, 1);
+INSERT INTO categories (`name`, `parent_id`, `level`) VALUES ('Смартфоны', 2, 2);
+INSERT INTO categories (`name`, `parent_id`, `level`) VALUES ('Xiaomi', 3, 3),('Meizu', 3, 3),('Samsung', 3, 3);
