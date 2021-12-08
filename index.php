dbdb822ba53240d9aab171070e0acead594be032
mihailmarkevich <mihail.markevich.nox@gmail.com>
2021-12-08 21:34:07 +0200

    Categories hierarchy collect example

--- index.php
+++ index.php
@@ -2,30 +2,27 @@
 
 $db = mysqli_connect('localhost', 'root', '', 'database');
 
-$query = mysqli_query($db, "
-               SELECT c.id, c.name, ctc.parent_category_id
-               FROM categories c 
-			   LEFT JOIN categories_to_categories ctc on c.name = ctc.name  
-			   ORDER BY ctc.level ASC");
+$query = mysqli_query($db, "SELECT * FROM categories ORDER BY `level` ASC");
 
 $categories = $query->fetch_all(MYSQLI_ASSOC);
 
 $result = [];
 
-foreach ($categories as $category) {
-    if(is_null($category['parent_category_id'])){
+foreach ($categories as $category){
+    if ($category['parent_id'] == 0){
         $result[$category['id']] = [
             'name' => $category['name'],
             'childrens' => [],
         ];
     } else {
-        $result = $this->recursion($result, $category['parent_category_id'], $category);
+        $result = recursion($result, $category);
     }
 }
 
-function recursion($categories, $parentId, $toWrite){
-    if (isset($categories[$parentId])) {
-        $categories[$parentId]['childrens'][$toWrite['id']] = [
+
+function recursion($categories, $toWrite){
+    if (isset($categories[$toWrite['parent_id']])){
+        $categories[$toWrite['parent_id']]['childrens'][$toWrite['id']] = [
             'name' => $toWrite['name'],
             'childrens' => []
         ];
@@ -33,11 +30,45 @@ function recursion($categories, $parentId, $toWrite){
         return $categories;
     }
 
-    foreach ($categories as $key => $category) {
-        $category['childrens'] = $this->recursion($category['childrens'], $parentId, $toWrite);
+    foreach ($categories as $key => $category){
+        $category['childrens'] = recursion($category['childrens'], $toWrite);
 
         $categories[$key] = $category;
     }
 
     return $categories;
 }
+
+function recursionResponse($categories){
+    $html = '<div class="categories">';
+
+    foreach ($categories as $category){
+        $html .= '<div class="category">
+                     <p>' . $category['name'] . '</p>';
+
+        if ($category['childrens']){
+            $html .= recursionResponse($category['childrens']);
+        }
+
+        $html .= '</div>';
+    }
+
+    $html .= '</div>';
+
+    return $html;
+}
+
+?>
+
+
+<html>
+    <style>
+        .category{
+            border: 1px solid silver;
+            padding: 10px;
+        }
+    </style>
+    <body>
+        <?php echo recursionResponse($result); ?>
+    </body>
+</html>
